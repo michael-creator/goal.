781479647850abccad865441f5764fe1235d0c7e
michael <mikelkarije@gmail.com>
2019-10-09 11:49:47 +0300

    this commit adds description to the quotes

--- angular/project/Quotes/src/app/quote/quote.component.ts
+++ angular/project/Quotes/src/app/quote/quote.component.ts
@@ -9,11 +9,11 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] =[
-    {id:1, name:'Smile because it confuses people. Smile because its easier than explaining what is killing you inside.'},
-    {id:2, name:'Introduce a little anarchy. Upset the established order, and everything becomes chaos. Im an agent of chaos...'},
-    {id:3, name:'Do I really look like a guy with a plan? You know what I am? Im a dog chasing cars. I wouldnt know what to do with one if I caught it! You know, I just... *do* things.' },
-    {id:4, name:'Why so serious? >:)'},
-    {id:5, name:'They Laugh At me Because Im Different. I laugh At Then Because There all the same'},
+    {id:1, name:'Smile because it confuses people. Smile because its easier than explaining what is killing you inside.',description: 'The Joker - Heath Ledger'},
+    {id:2, name:'Introduce a little anarchy. Upset the established order, and everything becomes chaos. Im an agent of chaos...',description: 'The Joker - Heath Ledger'},
+    {id:3, name:'Do I really look like a guy with a plan? You know what I am? Im a dog chasing cars. I wouldnt know what to do with one if I caught it! You know, I just... *do* things.' ,description: 'The Joker - Heath Ledger'},
+    {id:4, name:'Why so serious? >:)',description: 'The Joker - Heath Ledger'},
+    {id:5, name:'They Laugh At me Because Im Different. I laugh At Then Because There all the same',description: 'The Joker - Heath Ledger'},
   ];
   constructor() { }
 
